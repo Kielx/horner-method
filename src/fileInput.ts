0e17522364ada16db030bb4af0b1ad161745a70a
Krzysztof Pantak <kielx.dev@gmail.com>
2023-03-26 22:27:06 +0200

    Rework and restyle app

--- src/fileInput.ts
+++ src/fileInput.ts
@@ -1,12 +1,29 @@
+// Tablica przechowująca dane wejściowe
 let numberInputsArray: number[] = []
 
-export const setupFileInput: (element: HTMLInputElement) => void = (element)  => {
+// Obiekt przechowujący błędy
+const errors = {
+  noFile: {
+    message: 'Wybierz plik z danymi wejściowymi',
+    enabled: false
+  },
+  noX: {
+    message: 'Wybierz poprawną wartość X',
+    enabled: false
+  }
+}
+
+/**
+ * Funkcja, która nasłuchuje zmiany pliku wejściowego
+ *  
+ * @param element Element inputu, który ma być nasłuchiwany
+ */
+ export const setupFileInput: (element: HTMLInputElement) => void = (element)  => {
   return (
     element.addEventListener('change', (event: Event) => {
       if (!event.currentTarget) return
       else {
         const file = (event.currentTarget as HTMLInputElement)?.files?.[0]
-        console.log(file)
         if (file) readFile(file)
 
       }
@@ -15,22 +32,33 @@ export const setupFileInput: (element: HTMLInputElement) => void = (element)  =>
   
 }
 
+/**
+ * Funkcja, która odczytuje plik wejściowy
+ * 
+ * @param file Plik, który ma być odczytany
+ */
 const readFile = (file: File) => {
-  const inputResult = document.querySelector<HTMLDivElement>("#inputResult")!
   const reader = new FileReader()
   reader.addEventListener('load', (event) => {
     const inputs = event.target?.result
     const inputsArray = inputs?.toString().split(/\r?\n/)
-    if (inputsArray)
-    numberInputsArray = inputsArray?.map((input) => Number(input))
-    if (inputs) {
-      inputResult.innerHTML = inputs.toString()
+    if (inputsArray) {
+      numberInputsArray = inputsArray?.map((input) => Number(input))
+      updateResult()
     }
   })
   reader.readAsText(file)
 
 }
 
+/**
+ * Funkcja, która oblicza wartość wielomianu metodą Hornera
+ * 
+ * @param wielomian Tablica współczynników wielomianu
+ * @param n Stopień wielomianu
+ * @returns Wartość wielomianu
+ *  
+ */
 const horner = (wielomian: number[], n: number) =>
 {
  
@@ -46,14 +74,76 @@ const horner = (wielomian: number[], n: number) =>
     return res
 }
 
-export const setupUpdateResult = (element: HTMLInputElement) => {
-  element.addEventListener('change', (event: Event) => {
-    if (!event.currentTarget) return
-    else {
-      console.log('afdsd')
+/**
+ * Aktualizuje wynik obliczeń, wyświetla błędy jeśli istnieją
+ */
+const updateResult = () => {
   const result = horner(numberInputsArray, numberInputsArray.length)
   const resultDiv = document.querySelector<HTMLDivElement>("#result")!
-  resultDiv.innerHTML = `Wynik to: ${result}`
+  const errorsDiv = document.querySelector<HTMLDivElement>("#errors")!
+  const x = document.querySelector<HTMLInputElement>("#x")!.valueAsNumber
+
+  // Sprawdzenie czy wczytano poprawnie plik z danymi wejściowymi
+  // W przypadku błędu wyświetla odpowiedni komunikat
+  if (numberInputsArray.length === 0) {
+    if (!errors.noFile.enabled) {
+      errors.noFile.enabled = true
+      const newError = document.createElement('p')
+      newError.innerHTML = errors.noFile.message
+      newError.id = 'noFile'
+      errorsDiv.appendChild(newError)
+      errorsDiv.style.display = 'flex'
+      resultDiv.innerHTML = ''
     }
+    
+  }
+  // Usuwanie komunikatu o błędzie jeśli został poprawiony
+  else {
+    errors.noFile.enabled = false
+    const noFileError = document.querySelector<HTMLParagraphElement>("#noFile")
+    if (noFileError) {
+      errorsDiv.removeChild(noFileError)
+    }
+  }
+
+  // Sprawdzenie czy podano poprawną wartość X
+  // W przypadku błędu wyświetla odpowiedni komunikat
+  if (isNaN(x)) {
+    if (!errors.noX.enabled)  {
+      errors.noX.enabled = true
+      const newError = document.createElement('p')
+      newError.innerHTML = errors.noX.message
+      newError.id = 'noX'
+      errorsDiv.appendChild(newError)
+      errorsDiv.style.display = 'flex'
+      resultDiv.innerHTML = ''
+    }
+  // Usuwanie komunikatu o błędzie jeśli został poprawiony
+  } else {
+    errors.noX.enabled = false
+    const noXError = document.querySelector<HTMLParagraphElement>("#noX")
+    if (noXError) {
+      errorsDiv.removeChild(noXError)
+    }
+  }
+
+  // Wyświetlanie wyniku jeśli nie ma błędów
+  if (numberInputsArray.length !== 0 && !isNaN(x)) {
+    errors.noFile.enabled = false
+    errors.noX.enabled = false
+    errorsDiv.innerHTML = ''
+    errorsDiv.style.display = 'none'
+    resultDiv.innerHTML = `Wynik to: ${result}`
+  }
+}
+
+/**
+ * Funkcja, która nasłuchuje zmiany wartości inputu X
+ * 
+ * @param element Element inputu, który ma być nasłuchiwany
+ */
+export const setupUpdateResult = (element: HTMLInputElement) => {
+  element.addEventListener('change', () => {
+    updateResult()
   })
 }
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,20 @@ import  {setupFileInput, setupUpdateResult} from "./fileInput";
 import "./style.css";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-     Hello World
-     <input type="file" id="file" />
+  <main>
+     <h1>Obliczanie wartości funkcji wielomianowej dla zadanej wartości argumentu - metodą Hörner’a.</h1>
+     <div id="filePickerContainer">
+     <label for="file">Wybierz plik z danymi wejściowymi</label>
+     <input type="file" id="file" accept=".txt" />
+     </div>
      <div id="inputResult"></div>
-     <input type="number" id="x" />
-     <div id="result">Result:</div>
-  </div>
+     <div id="xContainer">
+     <label for="x">Podaj wartość X</label>
+     <input type="number" id="x" value="0" />
+     </div>
+     <div id="errors"></div>
+     <div id="result">Wybierz plik z danymi wejściowymi oraz podaj wartość X by obliczyć wartość funkcji wielomianowej</div>
+  </main>
 `;
 
 setupFileInput(document.querySelector<HTMLInputElement>("#file")!);
